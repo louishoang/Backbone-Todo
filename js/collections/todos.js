7807fb6b08fe26c5150c05fb1acdaad641d9d02b
Louis Hoang <bawsing@gmail.com>
2015-11-01 11:50:26 -0800

    added collection of todos

--- js/collections/todos.js
+++ js/collections/todos.js
@@ -1,1 +1,38 @@
-todos.js
+// js/collections/todos.js
+var app = app || {};
+
+var TodoList = Backbone.Collection.extend({
+  //reference to this collection's model
+  model: app.Todo,
+
+  // Save all of the todo items under the `"todos-backbone"` namespace.
+  localStorage: new Backbone.LocalStorage('todos-backbone'),
+
+  completed: function(){
+    return this.filter(function(todo){
+      return todo.get('completed');
+    });
+  },
+
+  remaining: function(){
+    return this.without.apply(this, this.completed());
+  },
+
+  // We keep the Todos in sequential order, despite being saved by unordered
+  // GUID in the database. This generates the next order number for new items.
+
+  nextOrder: function() {
+    if ( !this.length ) {
+      return 1;
+    }
+    return this.last().get('order') + 1;
+  },
+
+  // Todos are sorted by their original insertion order.
+  comparator: function( todo ) {
+    return todo.get('order');
+  }
+});
+
+// Create our global collection of **Todos**.
+  app.Todos = new TodoList();
