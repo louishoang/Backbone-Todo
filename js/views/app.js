bbe37348f57e0078cfa000d146aec9f1c72a4805
Louis Hoang <bawsing@gmail.com>
2015-11-01 13:34:31 -0800

    added view

---
+++ js/views/app.js
@@ -0,0 +1,27 @@
+var app = app || {};
+
+app.AppView = Backbone.View.extend({
+  el: "todoapp",
+
+  statsTemplate: _.template( $('#stats-template').html() ),
+
+  initialize: function(){
+    this.allCheckbox = this.$('#toggle-all')[0];
+    this.$input = this.$('#new-todo');
+    this.$footer = this.$('#footer');
+    this.$main = this.$('#main');
+
+    this.listenTo(app.Todos, 'add', this.addOne);
+    this.listenTo(app.Todos, 'reset', this.addAll); 
+  },
+
+  addOne: function(todo){
+    var view = newapp.TodoView({ model: todo });
+    $("#todo-list").append( view.render().el );
+  },
+
+  addAll: function() {
+    this.$('#todo-list').html('');
+    app.Todos.each(this.addOne, this);
+  }
+});
